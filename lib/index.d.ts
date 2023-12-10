2755bcd63435ff0c1db32814f510941ea7190d27
Jamiu Adeniyi <30894401+lyricsking@users.noreply.github.com>
2023-12-10 17:41:12 +0000

    Export types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,9 @@ import { AxiosInstance } from "axios";
 import OrderService from "./order/order.service";
 import CarrierService from "./carrier/carrier.service";
 import OnDemandService from "./ondemand.delivery/on.demand.delivery.service";
+export * from "./carrier/carrier.request";
+export * from "./ondemand.delivery/ondemand.types";
+export * from "./order/order.types";
 export default class Shipday {
     axiosClient: AxiosInstance;
     orderService: OrderService;
